c2e298cbfb15f846d9b302a7dfb339092120bd61
xxih <2217911664@qq.com>
2022-01-07 11:19:28 +0800

    feat: 修改路由，删除无用路由。

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import {HashRouter,Route,Switch} from 'react-router-dom'
 import Login from '../views/LoginPage/Login.js'
-import CommentPage from '../views/CommentPage/CommentPage.js'
-import RatePage from '../views/RatePage/RatePage.js'
 import GuideResult from '../views/GuideResult/GuideResult'
 
 export default function IndexRouter() {
@@ -10,10 +8,8 @@ export default function IndexRouter() {
     <HashRouter>
       <Switch>
         <Route path='/login' component={Login}></Route>
-        <Route path='/commentPage' component={CommentPage}></Route>
-        <Route path='/ratePage' component={RatePage}></Route>
         <Route path='/guideResult' component={GuideResult}></Route>
-        <Route path='/' component={CommentPage}></Route>
+        <Route path='/' component={GuideResult}></Route>
       </Switch>
     </HashRouter>
 
